b253fdd10c6da65174c69b731879e7f4b431a559
binonhq <nhquan.239@gmail.com>
2023-08-19 16:50:40 +0700

    update

--- api/models/ComboModel.js
+++ api/models/ComboModel.js
@@ -6,16 +6,17 @@ const comboSchema = new mongoose.Schema({
     imageUrl: { type: String, required: true },
     comboData: {
         pizzas: [{
-            _id: {type: mongoose.Schema.Types.ObjectId, ref: 'Pizza'},
-            size: {type: String, enum: ['S', 'M', 'L']},
-            toppings: [{type: mongoose.Schema.Types.ObjectId, ref: 'Topping'}],
-            quantity: {type: Number, min: 0}
+            _id: { type: mongoose.Schema.Types.ObjectId, ref: 'Pizza' },
+            size: { type: String, enum: ['S', 'M', 'L'] },
+            toppings: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Topping' }],
+            quantity: { type: Number, min: 0 }
         }],
         sideDishes: [{
-            _id: {type: mongoose.Schema.Types.ObjectId, ref: 'SideDish'},
-            quantity: {type: Number, min: 0}
+            _id: { type: mongoose.Schema.Types.ObjectId, ref: 'SideDish' },
+            quantity: { type: Number, min: 0 }
         }]
     },
+    category: { type: String, immutable: true, default: 'combo' },
     price: { type: Number, min: 0, required: true },
     orderCount: { type: Number, min: 0, default: 0 }
 }, {
