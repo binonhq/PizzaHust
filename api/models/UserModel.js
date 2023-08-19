6b91885361043bd29a49869b0a3a96e5171fe94c
namtotbung <nam.nn265@gmail.com>
2023-08-19 08:16:09 +0700

    add validation to UserModel

--- api/models/UserModel.js
+++ api/models/UserModel.js
@@ -2,13 +2,23 @@ const mongoose = require('mongoose');
 const crypto = require('crypto');
 
 const userSchema = new mongoose.Schema({
-    username: {type: String, required: true, immutable: true},
+    username: {type: String, required: true, immutable: true, unique: true},
     passwordHash: {type: String, required: true},
     salt: {type: String, required: true, immutable: true},
     isAdmin: {type: Boolean, required: true, default: false},
     name: {type: String, required: true},
-    email: {type: String, required: true},
-    phoneNumber: {type: String, required: true},
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/
+    },
+    phoneNumber: {
+        type: String,
+        required: true,
+        unique: true,
+        match: /^\d{10}$/
+    },
     address: {type: String, required: true},
     orders: [{type: mongoose.Schema.Types.ObjectId, ref: 'Order'}],
     status: { type: String, enum: ['active', 'blocked'], default: 'active'},
